e6dd2d0ba7b8672f6b1e6c4fe7f422d7270fbb1b
Rui Carmo <rui.carmo@gmail.com>
2019-05-21 22:51:21 +0100

    battle testing

--- bundles/slim/init.sh
+++ bundles/slim/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # This is already baked into our base image, no point in setting up another
-USER=node 
+USER=node
 
 # Follow the linuxserver.io approach to setting UIDs
 PUID=${PUID:-911}
@@ -14,7 +14,7 @@ usermod -o -u "$PUID" $USER
 usermod -d /usr/src/node-red $USER
 
 chown $USER:$USER /data
-chown $USER:$USER /usr/src/node-red
+chown -R $USER:$USER /usr/src/node-red
 
 # CMD
-su -c "/usr/local/bin/npm start -- --userDir /data" -p $USER
+sudo -H -E -u $USER /usr/local/bin/npm start -- --userDir /data
