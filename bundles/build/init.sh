4670cb5f2ff86179823df989a623541fa6672c5b
Rui Carmo <rui.carmo@gmail.com>
2020-06-08 16:15:25 +0000

    freeze

--- bundles/build/init.sh
+++ bundles/build/init.sh
@@ -14,7 +14,7 @@ usermod -o -u "$PUID" $USER
 usermod -d /usr/src/node-red $USER
 
 chown -R $USER:$USER /data
-#chown -R $USER:$USER /usr/src/node-red
+chown -R $USER:$USER /usr/src/node-red
 
 # CMD
 sudo -H -E -u $USER /usr/local/bin/npm start -- --userDir /data
