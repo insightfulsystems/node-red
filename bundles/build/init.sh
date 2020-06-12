fef81a92cd6eef5b5337fb2ad117cb3a76dc174c
Rui Carmo <rui.carmo@gmail.com>
2020-06-12 11:39:10 +0000

    permission tweaks

--- bundles/build/init.sh
+++ bundles/build/init.sh
@@ -9,12 +9,15 @@ PGID=${PGID:-911}
 
 echo "UID:GID - $PUID:$PGID"
 
+echo "Setting user permissions..."
+
 groupmod -o -g "$PGID" $USER
 usermod -o -u "$PUID" $USER
 usermod -d /usr/src/node-red $USER
+chown -R $PUID:$PGID /data
+chown -R $PUID:$PGID /usr/src/node-red
 
-chown -R $USER:$USER /data
-chown -R $USER:$USER /usr/src/node-red
+echo "Starting..."
 
 # CMD
 sudo -H -E -u $USER /usr/local/bin/npm start -- --userDir /data
